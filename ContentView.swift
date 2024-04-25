f23ecb275b061394f470bc0251086c04cb214c49
Pavel Sliziuk <psliziuk6381@stu.d214.org>
2024-04-25 13:12:02 -0500

    added the logo on the main screen and other stuff

--- ContentView.swift
+++ ContentView.swift
@@ -3,12 +3,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+          Image("val esport logo")
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             NavigationView{
                 NavigationLink("Go to YouTubeKit", destination: YouTubeKit())
-                
             }
         
         }
-        
+        navigationTitle("Welcome To Validictions")
     }
 }
