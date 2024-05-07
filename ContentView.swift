7aabeb39e9188a779826779cc14d63358cef68e1
Pavel Sliziuk <psliziuk6381@stu.d214.org>
2024-05-07 13:29:17 -0500

    Added ontaps for content view

--- ContentView.swift
+++ ContentView.swift
@@ -2,13 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State var selectedTab = 0
+    
     var body: some View {
         VStack{
-            NavigationView {
-                NavigationLink("Go to TeamsView", destination: TeamsView())
-                NavigationLink("Go to YoutubeKit", destination: YouTubeKit())
+            TabView(selection: $selectedTab) {
+                        Text("Go To YoutubeKit")
+                            .tabItem {
+                                Image(systemName: "1.circle")
+                                Text("First")
+                            }
+                            .onTapGesture{
+                                YouTubeKit()
+                            }
+                    
+                        Text("Go To Teams View")
+                            .tabItem {
+                                Image(systemName: "2.circle")
+                                Text("Second")
+                                
+                            }
+                            .onTapGesture{
+                                TeamsView()
+                            }
             }
         }
-        .navigationTitle("Welcome To Validictions")
     }
 }
